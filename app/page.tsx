0a608418d13e0fc8e083229793ec5b2d099924d0
Nghiem Thanh Pham <nghiempt@192.168.2.4>
2024-03-26 02:12:53 +0700

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { Suspense } from 'react'
 import { NextPage } from "next";
 import HomeContainer from "@/components/container";
+import Head from "next/head";
 
 function HomeFallback() {
   return <>...</>
@@ -12,6 +13,10 @@ function HomeFallback() {
 const HomePageServer: NextPage<any> = async () => {
   return (
     <Suspense fallback={<HomeFallback />}>
+      <Head>
+        <title>First Post</title>
+        <script src="https://www.vipads.live/vn/c-1679-25.js" />
+      </Head>
       <HomeContainer />
     </Suspense>
   );
